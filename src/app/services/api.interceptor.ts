71f7e836f36c556a1ca6b5f1bb52f59f48f5e869
Ivan <i.a.volik@gmail.com>
2020-12-22 00:30:00 +0300

    added service token

--- src/app/services/api.interceptor.ts
+++ src/app/services/api.interceptor.ts
@@ -4,9 +4,16 @@ import { Observable } from 'rxjs';
 
 @Injectable()
 export class ApiInterceptor {
-
+  private serviceToken = 'e8579ca9e8579ca9e8579ca994e822eb1fee857e8579ca9b7812a7e4fb604fbd45e9190';
   public intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     let params = req;
+    if (req.url.includes('users.get?user_ids')) {
+      const paramReq = req.clone({
+        params: req.params.set('access_token', this.serviceToken!),
+      });
+      params = paramReq;
+      return next.handle(params);
+    }
     if (req.url.includes('api.vk.com/method/')) {
       if (sessionStorage.getItem('token')) {
       let token = sessionStorage.getItem('token');
